faae445435350ba737efdd7a7e907e8acd368a35
Iratu <admin@rori-dl.com>
2015-12-18 02:26:25 +0100

    COuld it be this simple?

--- app/models/post.rb
+++ app/models/post.rb
@@ -587,8 +587,8 @@ class Post < ActiveRecord::Base
 	
 	def check_tagme(tags)
 	  if tags.split.size > 10 && tags.include?("tagme")
-		tags.gsub! 'tagme', ''
-		#tags >> "tagme"
+		#tags.gsub! 'tagme', ''
+		tags >> "tagme"
 	  end
 	  if tags.split.size < 10 && !tags.include?("tagme")
 		tags << "tagme"
