ebdbb66c9c078ffdbf524c2956bd55a47a82d046
Iratu <admin@rori-dl.com>
2016-12-12 00:02:35 +0100

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,9 @@
 set :stages, %w(production development staging)
 set :default_stage, "staging"
 set :application, "danbooru"
-set :repo_url,  "https://github.com/Iratu/danbooru.git"
+set :repo_url,  "https://github.com/Iratu/atfbooru.git"
 set :scm, :git
+set :branch, "master"
 set :deploy_to, "/var/www/danbooru2"
 set :rbenv_ruby, "2.3.1"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
