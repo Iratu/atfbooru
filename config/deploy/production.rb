32dcd5327d330d0a8e45205358c19b44b3b285b4
Iratu <admin@rori-dl.com>
2016-12-17 00:53:56 +0100

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
 set :user, "danbooru"
 set :rails_env, "production"
-server "127.0.0.1", :roles => %w(web app db), :primary => true, :user => "danbooru"
+server "iratu.pro", :roles => %w(web app db), :primary => true, :user => "danbooru"
