0153aaca3c34abbd68144692d7ef6ec62ad04af3
Iratu <admin@rori-dl.com>
2018-03-01 20:56:06 +0100

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ set :user, "danbooru"
 set :rails_env, "production"
 server "localhost", :roles => %w(web app db), :primary => true, :user => "danbooru"
 set :linked_files, fetch(:linked_files, []).push(".env.production")
-set :rbenv_path, "/home/albert/.rbenv"
+set :rbenv_path, "/home/danbooru/.rbenv"
