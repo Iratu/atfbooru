1ac28712654fc98f5e6e90fe4f366cceb7e5e659
Iratu <admin@rori-dl.com>
2018-04-16 23:26:51 +0200

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,6 @@ set :repo_url,  "https://github.com/Iratu/atfbooru.git"
 set :scm, :git
 set :branch, "master"
 set :deploy_to, "/var/www/danbooru2"
-set :rbenv_ruby, "2.5.0
+set :rbenv_ruby, "2.5.0"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
 set :branch, ENV.fetch("branch", "master")
